4a4c681bb4860bcd558931496d10e1bdb8f7b1a1
Mr-Tinkerman <37818934+Mr-Tinkerman@users.noreply.github.com>
2018-04-20 00:00:57 -0600

    Create mimic_bot.js

---
+++ mimic_bot.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config_file = require("./config.json");
+var prefix = config_file.prefix;
+
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  var args = message.content.slice(prefix.length).trim().split(/ +/g);
+  var command = args.shift().toLowerCase();
+
+  if (command === "Mimic") 
+  {
+    message.channel.send("You are no longer being mimiced.");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
